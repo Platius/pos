1fd1c60d703615f5beb7ac42208621edea124f74
Ivan Alekseev <i.a.alexeev@gmail.com>
2015-03-31 19:08:11 +0300

    Обновление клиентских библиотек

--- bin/Http/com-unregister.bat
+++ bin/Http/com-unregister.bat
@@ -1,5 +1,5 @@
-if %PROCESSOR_ARCHITECTURE%==x86 (
+rem if %PROCESSOR_ARCHITECTURE%==x86 (
   "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\regasm.exe" iikoNet.Service.Api.Front.v2.Client.dll /codebase /unregister
-) else (
-  "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe" iikoNet.Service.Api.Front.v2.Client.dll /codebase /unregister
-)
+rem ) else (
+rem  "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe" iikoNet.Service.Api.Front.v2.Client.dll /codebase /unregister
+rem )
