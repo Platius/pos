798f39717e84ee794cfb805b17cdde02a12012a3
Ivan Alekseev <i.a.alexeev@gmail.com>
2015-04-09 11:40:11 +0300

    Исправление примера для Delphi

--- samples/delphi/Unit1.pas
+++ samples/delphi/Unit1.pas
@@ -1,4 +1,4 @@
-﻿unit Unit1;
+unit Unit1;
 
 interface
 
@@ -36,7 +36,6 @@ begin
   i_startupParams := CoStartupParams.Create();
   i_Flow := CoWorkflowImpl.Create();
 
-  //�������������
   i_startupParams.Host := 'msk-dborisov.resto.lan';
 //  i_startupParams.Host := 'http://www4.iiko.net:9010';
   i_startupParams.Login := '7796';
@@ -87,16 +86,13 @@ begin
   discountsFlag := false;
   paymentsFlag := false;
   try
-    //������������� �������
     userPhone := '+79169059546';
 
-    //����� ����
     i_order := i_Flow.CreateOrder();
     guid := i_Flow.NewGuid;
     i_order.Id := guid;
     i_order.Number := '444';
 
-    //�������
     for i := 1 to 3 do begin
       i_item := i_Flow.CreateOrderItem();
       i_item.ProductCode := IntToStr(i*100) + '100';
@@ -108,14 +104,11 @@ begin
       i_item.ProductCategory := 'Category'  + IntToStr(i);
       i_order.AddItem(i_item);
     end;
-    //����� ����� �����
     i_order.Sum := 6212;
-    //����� ����� ����� � ������ ������ ���������
     i_order.SumAfterDiscount := 6106;
 
     minSum := i_order.Sum;
 
-    //�����������
     i_productLimits := i_Flow.CreateParamsCollection();
     i_productLimit := i_Flow.CreateProductLimit();
     i_productLimit.ProductCode := '100100';
@@ -125,21 +118,15 @@ begin
 
     i_productLimit := i_Flow.CreateProductLimit();
     i_productLimit.ProductCode := '200100';
-    //�������� �� ����� ���� ������� ��������
     i_productLimit.MaxBonusPayment := 0;
-    //���� �� ����� ����� ������� ��������� ��������
-    //����� �� �� �� ������ �� ���������, ��� �����?
     i_productLimit.MaxDiscount := 100;
     i_productLimits.Add(i_productLimit);
 
-    //�������� ����
     i_checkinResult := i_Flow.Checkin(userPhone, 1, i_order, i_productLimits);
-    // ���� ��������
     if (i_checkinResult.UserData.GetBirthday = Now) then
       ShowMessage('Happy birthday to you, dear ' + i_checkinResult.UserData.FullName);
 
     i_checkinResult := i_Flow.Precheque(i_order, i_productLimits);
-    //���������� ���������� �� iiko ������ �� �������
     for i := 0 to i_checkinResult.LoyaltyResult.Programs.Count - 1 do begin
 
       i_programResult := i_checkinResult.LoyaltyResult.Programs.Get(i) as ILoyaltyProgramResult;
@@ -147,46 +134,33 @@ begin
       for j := 0 to i_programResult.Operations.Count - 1 do begin
         i_operation := i_programResult.Operations.Get(i) as ILoyaltyOperation;
         productCode := i_operation.ProductCode;
-        //i_operation.Code ���� ��������� � ������ �� �������
         discountCode := i_operation.Code;
         discountSum := i_operation.DiscountSum;
-        //���� - ���� �� ������ �� ����
         discountsFlag := discountsFlag or (discountSum <> 0);
       end;
     end;
 
-    //���� - ���� �� ������ �� ����
     discountsFlag := discountsFlag or (discountSum <> 0);
 
-    //��������
     paymentLimits := i_checkinResult.PaymentLimits;
     for i := 0 to paymentLimits.Count - 1 do begin
        i_paymentLimit := paymentLimits.Get(i) as IPaymentLimit;
-       //��� ��������
        walletCode := i_paymentLimit.WalletCode;
-       //������
        minSum := i_paymentLimit.MinSum;
        maxSum := i_paymentLimit.MaxSum;
        paymentsFlag := paymentsFlag or (maxSum <> 0);
     end;
 
-    //���������, ���� �� � ���� ��������, � ����� �� �� ��� �������
-    //��� ���-�� ������ � ���, ����� �� �� ������� �������� � � ������ �������� ��� ���
-    //���� ������ �������� ���� �� �����, �� ���������
 
-    paymentsFlag := paymentsFlag and (MessageBox(0, '������ �������������� ��������', '', MB_YESNO) = ID_YES);
-    //���� ���� � ����� ������ �� iiko ��� ������ ����� ������� via  iiko.net, �� �������� ������
     if (discountsFlag or paymentsFlag) then begin
       guid := i_order.Id;
       transactionGuid := i_Flow.NewGuid;
       i_payments := i_Flow.CreateParamsCollection();
       if (paymentsFlag) then begin
-        //������� �������� � ���������� �������� ��������(���������)
         i_walletPayment := i_Flow.CreateWalletPayment();
         i_walletPayment.WalletCode := WalletCode_Bonus;
         i_walletPayment.Sum := 1;
         i_payments.Add(i_walletPayment);
-        //������� �������� � ���������� �������� ��������(���������)
         i_walletPayment := i_Flow.CreateWalletPayment();
         i_walletPayment.WalletCode := WalletCode_PaymentCard;
         i_walletPayment.Sum := 2;
@@ -194,7 +168,6 @@ begin
       end;
       i_discounts := i_Flow.CreateParamsCollection();
       if discountsFlag then begin
-        //�������� � iiko ������� ����������� ������
         i_appliedDiscount := i_Flow.CreateAppliedDiscount();
         i_appliedDiscount.OperationCode := discountCode;
         i_appliedDiscount.ProductCode := '100100';
@@ -206,26 +179,18 @@ begin
         i_appliedDiscount.Sum := 0.3;
         i_discounts.Add(i_appliedDiscount);
       end;
-      //���� �������� �������/������ �������
       i_Flow.Pay(guid, transactionGuid, i_payments, i_discounts);
     end;
 
-    //��� ���-�� ������ ����������� ����
 
-    //��������� ���� �� iiko
     i_order.SetFiscalChequeNumber(223);
-    i_order.CashierName := '������� ������';
-    i_order.RestarauntSectionName := '��� 1';
     i_order.TableNumber := '7';
 
-    i_closeResult := i_Flow.Close(guid, 100);
 
     chequeFooter := i_closeResult.ChequeFooter;
 
-    //��������� ���� ����, �������� �������������� �����
     ShowMessage(chequeFooter);
 
-    //ShowMessage('��� Ok');
   finally
     i_order := nil;
     i_checkinResult :=  nil;
@@ -265,13 +230,11 @@ var
   ClientId : String;
 begin
   try
-    //����� ����,
     i_order := i_Flow.CreateOrder();
     guid := i_Flow.NewGuid;
     i_order.Id := guid;
     i_order.Number := '444';
 
-    //�������
     for i := 1 to 3 do begin
       i_item := i_Flow.CreateOrderItem();
       i_item.ProductCode := IntToStr(i*100) + '100';
@@ -283,13 +246,10 @@ begin
       i_item.ProductCategory := 'Category'  + IntToStr(i);
       i_order.AddItem(i_item);
     end;
-    //����� ����� �����
     i_order.Sum := 6212;
-    //����� ����� ����� � ������ ������ ���������
     i_order.SumAfterDiscount := 6106;
 
 
-   //�����������
     i_productLimits := i_Flow.CreateParamsCollection();
     i_productLimit := i_Flow.CreateProductLimit();
     i_productLimit.ProductCode := '100100';
@@ -299,38 +259,28 @@ begin
 
     i_productLimit := i_Flow.CreateProductLimit();
     i_productLimit.ProductCode := '200100';
-    //�������� �� ����� ���� ������� ��������
     i_productLimit.MaxBonusPayment := 0;
-    //���� �� ����� ����� ������� ��������� ��������
-    //����� �� �� �� ������ �� ���������, ��� �����?
     i_productLimit.MaxDiscount := 100;
     i_productLimits.Add(i_productLimit);
 
-    //�������� ����
     i_checkinResult := i_Flow.Precheque(i_order, i_productLimits);
     //operations := i_complexFlow.GetPayments(nil);
-    ShowMessage('������: ' + i_checkinResult.ChequeFooter);
 
-    //���������� ���������� �� iiko ������ �� �������
     if i_checkinResult.LoyaltyResult.Programs <> nil then begin
       for i := 0 to i_checkinResult.LoyaltyResult.Programs.Count - 1 do begin
          i_programResult := i_checkinResult.LoyaltyResult.Programs.Get(i) as ILoyaltyProgramResult;
          for j := 0 to i_programResult.Operations.Count - 1 do begin
            i_operation := i_programResult.Operations.Get(j) as ILoyaltyOperation;
            productCode := i_operation.ProductCode;
-           //i_operation.OperationId ���� ��������� � ������ �� �������
            discountId := i_operation.Code;
            discountSum := i_operation.DiscountSum;
-           //���� - ���� �� ������ �� ����
            discountsFlag := discountsFlag or (discountSum <> 0);
          end
       end;
     end;
-    //���� - ���� �� ������ �� ����
     discountsFlag := discountsFlag or (discountSum <> 0);
 
 
-    //���� �� ������, ��� ������������� ��������
     if i_checkinResult.payments <> nil then begin
       for i := 0 to i_checkinResult.payments.Count - 1 do begin
          i_walletPayment := i_checkinResult.payments.Get(i) as IWalletPayment;
@@ -344,12 +294,10 @@ begin
       transactionGuid := i_Flow.NewGuid;
       i_payments := i_Flow.CreateParamsCollection();
       if (paymentsFlag) then begin
-        //������� �������� � ���������� �������� ��������(���������)
         i_walletPayment := i_Flow.CreateWalletPayment();
         i_walletPayment.WalletCode := WalletCode_Bonus;
         i_walletPayment.Sum := 100;
         i_payments.Add(i_walletPayment);
-        //������� �������� � ���������� �������� ��������(���������)
         i_walletPayment := i_Flow.CreateWalletPayment();
         i_walletPayment.WalletCode := WalletCode_PaymentCard;
         i_walletPayment.Sum := 200;
@@ -357,7 +305,6 @@ begin
       end;
       i_discounts := i_Flow.CreateParamsCollection();
       if discountsFlag then begin
-        //�������� � iiko ������� ����������� ������
         i_appliedDiscount := i_Flow.CreateAppliedDiscount();
         i_appliedDiscount.OperationCode := 'Discount 1';
         i_appliedDiscount.ProductCode := '100100';
@@ -370,21 +317,14 @@ begin
         i_appliedDiscount.Sum := 0.3;
         i_discounts.Add(i_appliedDiscount);
       end;
-      //���� �������� �������/������ �������
       i_Flow.Pay(guid, transactionGuid, i_payments, i_discounts);
     end;
 
-    //��� ���-�� ������ ����������� ����
 
-    //��������� ���� �� iiko
     i_order.SetFiscalChequeNumber(223);
-    i_order.CashierName := '������� ������';
-    i_order.RestarauntSectionName := '��� 1';
     i_order.TableNumber := '7';
 
     i_closeResult := i_Flow.Close(i_order.Id, 100);
-    //��������� ���� ����, �������� �������������� �����
-    ShowMessage('���: ' + i_closeResult.ChequeFooter);
   finally
     i_checkinResult :=  nil;
     i_order := nil;
