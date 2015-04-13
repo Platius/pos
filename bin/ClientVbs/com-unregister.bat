b20e225e56c9eee8a7880776a96f91779c3c3dae
Ivan Alekseev <i.a.alexeev@gmail.com>
2015-04-13 18:25:50 +0300

    Обновление клиента

--- bin/ClientVbs/com-unregister.bat
+++
@@ -1,5 +0,0 @@
-rem if %PROCESSOR_ARCHITECTURE%==x86 (
-  "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\regasm.exe" Platius.Service.Api.Front.v2.Client.dll /codebase /unregister
-rem ) else (
-rem  "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe" Platius.Service.Api.Front.v2.Client.dll /codebase /unregister
-rem )
